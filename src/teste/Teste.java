5f1f2d2c8846fb6f888bfd1905ba6ded3607b129
André Felipe <Andr Felipe@DESKTOP-TE4800V>
2022-05-26 19:09:30 -0300

    o teste foi um sucesso final

--- src/teste/Teste.java
+++ src/teste/Teste.java
@@ -15,7 +15,7 @@ public class Teste {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("ola mundo");
+        System.out.println("ola mundo ultima versao");
     }
     
 }
